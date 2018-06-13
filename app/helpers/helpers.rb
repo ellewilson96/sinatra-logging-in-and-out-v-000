a2ad5250acb7213c36af20cbc44c2270ae7245a7
Elle Wilson <eleanorwilsonn@gmail.com>
2018-06-13 22:02:05 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,9 @@
 class Helpers
-  def current_user(session)
-    user = @session[:user_id]
-    user
+  def self.current_user(session)
+    @user = User.find_by_id(session[:user_id])
   end
 
-  def is_logged_in?
+  def is_logged_in?(session)
+    if session[:user_id] =
   end
 end
