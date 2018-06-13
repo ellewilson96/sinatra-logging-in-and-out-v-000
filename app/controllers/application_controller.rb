47e8ca888fad77b055dd7485a2dda12352d9dc5d
Elle Wilson <eleanorwilsonn@gmail.com>
2018-06-13 21:45:13 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-
+    User.create(name: params[:name], email: params[:email], password: params[:password])
   end
 
   get '/logout' do
